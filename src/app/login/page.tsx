000fdf448812d3d2252a84743381a52946cf84f7
nnutica_ <nitin4t.lor@gmail.com>
2025-06-07 01:00:45 +0700

    Refactor code structure for improved readability and maintainability

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Image from "next/image";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { login } from "@/libs/authService";
@@ -21,48 +21,65 @@ export default function Login() {
     };
 
     return (
-        <div className="flex  h-screen bg-gray-100">
-            <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
-                <h1 className="text-2xl font-bold mb-4">Login</h1>
-                {error && <p className="text-red-500 mb-4">{error}</p>}
-                <form onSubmit={handleLogin}>
-                    <div className="mb-4">
-                        <label className="block text-sm font-medium text-gray-700">Email</label>
-                        <input
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="w-full border rounded-lg px-3 py-2"
-                            required
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-sm font-medium text-gray-700">Password</label>
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="w-full border rounded-lg px-3 py-2"
-                            required
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="w-full bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg"
-                    >
-                        Login
-                    </button>
-                </form>
-                <div className="mt-4 text-center">
-                    <p className="text-sm text-gray-600">Don't have an account?</p>
-                    <button
-                        onClick={() => router.push("/register")}
-                        className="text-blue-600 hover:underline"
-                    >
-                        Go to Register
-                    </button>
-                </div>
+        <div className="flex h-screen bg-sky-300">
+            <div className="hidden lg:block relative flex-[6] rounded-4xl">
+                <Image
+                    src="/achiraya.png"
+                    alt="thumbnail page"
+                    fill
+                    style={{
+                        objectFit: "fill",
+                        borderRadius: "1.5rem",
+                    }}
+                />
             </div>
-        </div>
+            <div className="bg-sky-300 flex-[6] p-8 sm:p-20 md:p-32 lg:p-36 w-full">
+                <div className="bg-white rounded-4xl p-8 sm:p-12 md:p-16 lg:p-20 shadow-lg">
+                    <h1 className="text-2xl font-bold mb-4">Login</h1>
+                    {error && <p className="text-red-500 mb-4">{error}</p>}
+                    <form onSubmit={handleLogin}>
+                        <div className="mb-4">
+                            <label className="block text-sm font-medium text-gray-700">Email</label>
+                            <input
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="w-full border rounded-lg px-3 py-2"
+                                required
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-sm font-medium text-gray-700">Password</label>
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="w-full border rounded-lg px-3 py-2"
+                                required
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg"
+                        >
+                            Login
+                        </button>
+                    </form>
+                    <div className="mt-4 text-center">
+                        <p className="text-sm text-gray-600">Don't have an account?</p>
+                        <button
+                            onClick={() => router.push("/register")}
+                            className="text-blue-600 hover:underline"
+                        >
+                            Go to Register
+                        </button>
+                    </div>
+
+
+
+                </div>
+
+            </div>
+        </div >
     );
 }
